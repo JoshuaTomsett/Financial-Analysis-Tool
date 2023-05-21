ba793a34c9ef99f4f83012da6d41f74d14df968b
Josh <josh.james.tomsett@gmail.com>
2023-05-21 20:04:00 +0100

    Least squares in python

--- Least squares.py
+++ Least squares.py
@@ -1,2 +1,34 @@
-import numpy
+import numpy as np
+from matplotlib import pyplot as plt
 
+
+def calcRegressionLine(x, y):
+    """
+    Calculates the regression line using the least squares method.
+
+    Args:
+        x: Array of x values
+        y: Array of y values
+
+    Returns:
+        The slope (a) and intercept (b) of the regression line (y = ax+ b)
+    """
+
+    n = len(x)
+    Sx = sum(x)
+    Sx2 = sum(i*i for i in x)
+    Sy = sum(y)
+    Sxy = sum(a * b for a, b in zip(x, y))
+
+    a = (n*Sxy - Sx*Sy) / (n*Sx2 - Sx**2)
+
+    b = (Sx2*Sy - Sx*Sxy) / (n*Sx2 - Sx**2)
+
+    return a,b
+
+
+x = [1, 2, 3, 4, 5]
+y = [2, 5, 3, 8, 7]
+
+a,b = calcRegressionLine(x, y)
+print(a,b)
